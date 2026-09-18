5423f0e0ff2bf342e1b675c5f20fb4bbd16a7b5a
nexauren1 <nexaurenstore@gmail.com>
2026-09-18 18:39:56 +0200

    fix: add search text watcher imports

--- app/src/main/java/com/auren/musicplayer/MainActivity.java
+++ app/src/main/java/com/auren/musicplayer/MainActivity.java
@@ -18,6 +18,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
