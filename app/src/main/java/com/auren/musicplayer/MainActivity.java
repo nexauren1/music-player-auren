21766464dee54fe6cebc75bae1605e5d63bbd208
nexauren1 <nexaurenstore@gmail.com>
2026-09-18 18:59:37 +0200

    fix: add Button import for Nexauren Challenge

--- app/src/main/java/com/auren/musicplayer/MainActivity.java
+++ app/src/main/java/com/auren/musicplayer/MainActivity.java
@@ -25,6 +25,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
